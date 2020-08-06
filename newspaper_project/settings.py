27062e67587bffb9858e4e08154a52840c9c0e61
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2020-08-06 10:19:10 +0600

    reset password by email integrated.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -107,6 +107,11 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'itnewspaperbd@gmail.com'
+EMAIL_HOST_PASSWORD = 'itnewspaperjoybangla'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
