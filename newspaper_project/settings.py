b105986b485531e47c2edeb8cd08983e5ccf9eab
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2020-08-10 10:16:42 +0600

    google reset password is configured!

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -110,7 +110,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'itnewspaperbd@gmail.com'
-EMAIL_HOST_PASSWORD = 'itnewspaperjoybangla'
+EMAIL_HOST_PASSWORD = 'vtetexttsikucwys'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
